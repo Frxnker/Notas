07937dd41058499af7b6b5e7c15537a890f27b79
Francisco López Lobanova <128869320+Frxnker@users.noreply.github.com>
2024-09-30 13:01:12 +0200

    Add files via upload

--- Notas.java
+++ Notas.java
@@ -1,13 +1,12 @@
-public class Notas {
-    
+public class Notas {   
     public static void main(String[] args) {
 
         System.out.println("Introduce las notas de los alumnos: ");
-        double nota = 0;
+        double nota;
         int alumnos = 30;
 
         for (int i = 0; i <= alumnos; i++) {
-            nota = Integer.parseInt(System.console().readLine());
+            nota = Double.parseDouble(System.console().readLine());
             if (nota < 0 || nota > 10) {
                 System.out.println("Nota no válida");
             } else if (nota < 5) {
